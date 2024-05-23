e26c93a023af517f7b22fb3f01e7a441175be872
nfisherman <ca.lehman05@gmail.com>
2024-05-22 19:51:32 -0500

    fixed bug that prevented dropdowns from being closed by being clicked again

--- src/js/menubar.js
+++ src/js/menubar.js
@@ -1,18 +1,22 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function dropdown(element) {
-    hideAll("dropdown-content");
+    hideAll("dropdown-content", element);
     document.getElementById(element).classList.toggle("show");
 }
 
-function hideAll(domClass){
+function hideAll(domClass, ...exceptions) {
     let elements = document.getElementsByClassName("dropdown-content");
 
     for(const element of elements){
-        if (element.classList.contains('show')) {
+        if (element.classList.contains('show') && !exceptions.includes(element.id)) {
             element.classList.remove('show');
         }
     }
+}
+
+function hideExcept(domClass, exceptions) {
+    
 }
   
 // Close the dropdown menu if the user clicks outside of it
