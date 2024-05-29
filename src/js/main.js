806dae88a61a2bd2d575acbdf9e1518498d1a592
nfisherman <ca.lehman05@gmail.com>
2024-05-29 12:08:33 -0500

    added last.fm status and mobile menu bar collapsing

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 function randomize_logo() {
     const logo = document.getElementById('logo_img');
     
-    var dir = "/img/logo/";
-    var imgs = [
+    let dir = "/img/logo/";
+    let imgs = [
         "fishing_with_grandson_md_wht.gif",
         "fishingboat.gif",
         "fishingbobber.gif",
@@ -10,9 +10,44 @@ function randomize_logo() {
         "swimfish.gif"
     ]
 
-    var randImg = dir + imgs[
+    let randImg = dir + imgs[
         Math.floor(Math.random() * imgs.length)
     ];
 
     logo.src = randImg;
+}
+
+function expand_navbar(){
+    document.querySelector('#navexpand').style.display = "none";
+    document.querySelector('#nav').style.display = "block";
+}
+
+function collapse_navbar(){
+    document.querySelector('#navexpand').style.display = "block";
+    document.querySelector('#nav').style.display = "none";
+}
+
+function recently_played(username) {
+    let url = 'https://lastfm-last-played.biancarosa.com.br/' + username + '/latest-song';
+    
+    let a = document.querySelector('#widget');
+    let cover = document.querySelector('#cover');
+    let song = document.querySelector('#song');
+    let album = document.querySelector('#album');
+    let artist = document.querySelector('#artist');
+    fetch(url)
+        .then(function (response) {
+            return response.json()
+        }).then(function (json) {
+            a.href = json['track']['url'];
+            cover.src = json['track']['image'][1]['#text'];
+            song.innerHTML = json['track']['name'];
+            album.innerHTML = json['track']['album']['#text'];
+            artist.innerHTML = json['track']['artist']['#text'];
+        });
+}
+
+function main() {
+    randomize_logo();
+    recently_played("ingenuineangel");
 }
