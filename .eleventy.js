47854ec7959d69d8a6d6d45c714b3daf984fa98f
nfisherman <ca.lehman05@gmail.com>
2024-05-29 06:22:22 -0500

    added passthroughs for fonts, images, and js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css/");
+    eleventyConfig.addPassthroughCopy("./src/fonts/");
+    eleventyConfig.addPassthroughCopy("./src/img/");
+    eleventyConfig.addPassthroughCopy("./src/js/");
     
     return {
         dir: {
