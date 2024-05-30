552b981febae4d3ab8ea492e9df27e00f6906544
nfisherman <ca.lehman05@gmail.com>
2024-05-29 23:32:33 -0500

    compatibility check

--- pull-release.sh
+++ pull-release.sh
@@ -1,1 +1,24 @@
 #!/bin/sh
+
+#################
+### Variables ###
+#################
+
+REPO=""
+
+### Compatibility ###
+
+if command -v apt-get; then
+    echo
+fi    
+
+### Parse ###
+
+
+
+if nc -zw1 "$REPO" 443; then
+    echo "Connected"
+else
+    echo "Could not establish a network connection."
+    exit 1
+fi
