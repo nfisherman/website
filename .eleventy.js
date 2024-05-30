5970ab2af951f40e25cb6afd8116b248206fb612
nfisherman <ca.lehman05@gmail.com>
2024-05-29 22:35:51 -0500

    added html minification on build

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,60 @@
-module.exports = function (eleventyConfig) {
+require('dotenv').config();
+const htmlmin = require('html-minifier-terser');
+const jsmin = require('terser');
+
+module.exports = (eleventyConfig) => {
     eleventyConfig.addPassthroughCopy("./src/css/");
     eleventyConfig.addPassthroughCopy("./src/fonts/");
     eleventyConfig.addPassthroughCopy("./src/img/");
     eleventyConfig.addPassthroughCopy("./src/js/");
+
+    eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+        if(outputPath.endsWith(".html")) {
+            return htmlmin.minify(content, {
+                collapseBooleanAttributes: true,
+                collapseWhitespace: true,
+                removeComments: true,  
+                useShortDoctype: true
+            }).then(contentStr => {
+                return new Promise(function(resolve) {
+                    resolve(
+                        "<!-- HTML is minified. Clone from Github and run locally. -->\n" + 
+                        "<!-- https://github.com/nfisherman/nfisherman.com -->\n" +
+                        contentStr
+                    );
+                });
+            })
+        }
+
+        return content;
+    });
+
+    // eleventyConfig.addTransform("jsmin", (content, outputPath) => {
+    //     if(outputPath.endsWith(".js")) {
+    //         return jsmin.minify(content, {
+    //             toplevel: true,
+    //             compress: {
+    //                 global_defs: {
+    //                     "@console.log": "alert"
+    //                 },
+    //                 passes: 2
+    //             },
+    //             format: {
+    //                 preamble: "/* minified */"
+    //             }
+    //         }).then(contentStr => {
+    //             return new Promise(function(resolve) {
+    //                 resolve(
+    //                     "<!-- HTML is minified. Clone from Github and run locally. -->\n" + 
+    //                     "<!-- https://github.com/nfisherman/nfisherman.com -->\n" +
+    //                     contentStr
+    //                 );
+    //             });
+    //         })
+    //     }
+
+    //     return content;
+    // });
     
     return {
         dir: {
