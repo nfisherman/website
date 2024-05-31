c6d5a8c011e914aa80fb7f18c3ba7aee97b1295d
nfisherman <ca.lehman05@gmail.com>
2024-05-31 16:21:57 -0500

    added WTFPL license

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://www.wtfpl.net/ for more details.
+
 if ! command -v tar > /dev/null; then
     echo "tar: command not found"
     exit 1
