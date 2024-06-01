af40a2fb0971b3a35eb686288b5bd12e8547871b
nfisherman <ca.lehman05@gmail.com>
2024-06-01 14:59:25 -0500

    this script was meant to be much smaller so I didn't include the license notice, well now it's much bigger so here it is

--- tools/make-test-folder.sh
+++ tools/make-test-folder.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-set +x
+
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://www.wtfpl.net/ for more details.
 
 print_help() {
     echo "Usage: ./$(basename $0) {/path/to/output} [-p [\"args\"]] 
